fe3a0847973bfb17c269c3ef48524b92bdb9f221
tisontang(唐霜) <tisontang@tencent.com>
2021-09-28 13:09:56 +0800

    fix

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const options = require('process.args')(entry)
 const { port, tsconfig, root, webpackconfig } = options
 const cwd = process.cwd()
 const file = path.resolve(cwd, entry)
-const rootDir = path.resolve(cwd, root)
-const webpackConfigFile = path.resolve(cwd, webpackconfig)
+const rootDir = root ? path.resolve(cwd, root) : ''
+const webpackConfigFile = webpackconfig ? path.resolve(cwd, webpackconfig) : ''
 
 shell.cd(__dirname)
 shell.exec(`ENTRY="${file}" PORT=${port} CWD="${cwd}" TS_CONFIG="${tsconfig}" ROOT="${rootDir}" WEBPACK_CONFIG="${webpackConfigFile}" ./node_modules/webpack-dev-server/bin/webpack-dev-server.js --config=./source/webpack.config.js`)
